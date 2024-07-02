40c6a6d72af8cdd4619c3f42b8062de28856e06a
Afeni Samuel Oluwatimilehin <144561797+TimTech5@users.noreply.github.com>
2024-07-01 17:03:55 -0700

    Formula for Power Rule

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict'
 
-const areaOfCircle = function(radius){
-	return Math.PI * Math.pow(radius,2);
+function powerRule(figure, power) {
+    if (power === 0) {
+        return 0;
+    }
+    let newFigure = figure * power;
+    let newPower = power - 1;
+    return `${newFigure}x^${newPower}`;
 }
+
+
+let figure = 3; 
+let power = 8;    
+let derivative = powerRule(figure, power);
+console.log(`The derivative of ${figure}x^${power} is: ${derivative}`);
